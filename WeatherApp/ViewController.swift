63bcb4b850335feeac6d60d2af2ee6a97d28ec97
Márcio Souza de Oliveira <marciosouzadev@gmail.com>
2022-03-03 19:11:47 +0000

    feat: Add action sheet POC

--- WeatherApp/ViewController.swift
+++ WeatherApp/ViewController.swift
@@ -14,6 +14,7 @@ class ViewController: UIViewController {
         label.translatesAutoresizingMaskIntoConstraints = false
         label.font = .preferredFont(forTextStyle: .title1)
         label.text = NSLocalizedString("label", comment: "")
+        label.isUserInteractionEnabled = true
         return label
     }()
 
@@ -23,10 +24,33 @@ class ViewController: UIViewController {
         view.backgroundColor = .systemBackground
         view.addSubview(label)
 
+        let tapGesture = UITapGestureRecognizer(target: self,
+                                                action: #selector(createActionSheet))
+
+        label.addGestureRecognizer(tapGesture)
+
         NSLayoutConstraint.activate([
             label.centerXAnchor.constraint(equalTo: view.centerXAnchor),
             label.centerYAnchor.constraint(equalTo: view.centerYAnchor)
         ])
+    }
+
+    @objc private func createActionSheet() {
+        let optionMenu = UIAlertController(title: "Current weather for",
+                                           message: "The City",
+                                           preferredStyle: .actionSheet)
+
+        for option in 1...3 {
+            let action = UIAlertAction(title: "Test \(option)", style: .default)
+            action.isEnabled = false
+            optionMenu.addAction(action)
+        }
+
+        optionMenu.addAction(UIAlertAction(title: "Cancel", style: .cancel))
+
+        present(optionMenu, animated: true) {() -> Void in
+            optionMenu.view.superview?.subviews[0].isUserInteractionEnabled = false
+        }
     }
 }
 
